9aabae548b0b9faddac0b9ae635f549557f6c966
ChaoTzuJung <s110319022@stu.ntue.edu.tw>
2020-05-27 17:20:47 +0800

    compolete read data from SQL and send it to json data

--- resources/user.py
+++ resources/user.py
@@ -1,7 +1,7 @@
 # 處理 user 所有資源操作 - CRUD
 from flask_result import Response
 import pymysql
-# 建立 user 物件
+# 建立 user 物件, self 就是 this
 class Users():
     def db_init(self):
         # 連結料庫
@@ -9,3 +9,15 @@ class Users():
         # DictCursor 幫我們取資料時，把DB資料變成 key-value (1, 'John') -> {'id': 1, 'name': 'John'}
         cursor = db.cursor(pymysql.cursors.DictCursor)
         return db, cursor
+    # http get method
+    def get(self)
+        db, cursor = self.db_init()
+        sql = 'Select * From api.users'
+        cursor.execute(sql)
+        # db 送出
+        db.commit()
+        # 取得 cursor 所有資料
+        users = cursor.fetchall()
+        db.close()
+
+        return jsonify({ 'data': users })
