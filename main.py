c7dd5153193a285c2e9f1c6f215ae9aab5347cb4
ChaoTzuJung <s110319022@stu.ntue.edu.tw>
2020-06-12 17:56:09 +0800

    新增提款、匯款 api.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_restful import Api
 from resources.user import Users, User
 from resources.account import Accounts, Account
+import pymysql
+import traceback
 
 # 宣告是 flask app
 app = Flask(__name__) 
@@ -19,6 +21,60 @@ api.add_resource(Account, '/user/<user_id>/account/<id>')
 @app.route('/')
 def index():
     return 'Hello World'
+
+# 客製化路徑名稱 Ex: 
+@app.route('/user/<user_id>/account/<id>/deposit', methods=['POST'])
+# 為此路徑新增函示
+def deposit(user_id, id):
+    db, cursor, account = get_account(id)
+    money = request.get_json()['money']
+    balance = account['balance'] + int(money)
+    sql = """
+        UPDATE `api`.`accounts` SET balance = '{}' where id = '{}' and deleted is NOT True
+    """.format(balance, id)
+    response = {}
+    try:
+        cursor.execute(sql)
+        response['msg'] = 'success'
+    except:
+        traceback.print_exc()
+        response['msg'] = 'failed'
+    db.commit()
+    db.close()
+
+    return jsonify(response)
+
+@app.route('/user/<user_id>/account/<id>/withdraw', methods=['POST'])
+def withdraw(user_id, id):
+    db, cursor, account = get_account(id)
+    money = request.get_json()['money']
+    balance = account['balance'] - int(money)
+    response = {}
+    if balance < 0:
+        response['msg'] = 'money not enough'
+        return jsonify(response)
+    else:
+        sql = """
+            UPDATE `api`.`accounts` SET balance = '{}' where id = '{}' and deleted is NOT True
+        """.format(balance, id)
+        try:
+            cursor.execute(sql)
+            response['msg'] = 'success'
+        except:
+            traceback.print_exc()
+            response['msg'] = 'failed'
+        db.commit()
+        db.close()
+
+        return jsonify(response)
+
+def get_account(id):
+    db = pymysql.connect('localhost', 'root', 'password', 'api')
+    cursor = db.cursor(pymysql.cursors.DictCursor)
+    sql = """Select * From api.accounts Where id = '{}' and deleted is not True """.format(id)
+    cursor.execute(sql)
+    # cursor.fetchone() --> 回傳sql執行後得到的acount物件
+    return db, cursor, cursor.fetchone()
 
 if __name__ == '__main__':
     app.debug = True
